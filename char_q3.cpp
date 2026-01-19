7c93629876e78e888109dccf86f4691a0c461cf2
kzauha <kabishpok@gmail.com>
2026-01-19 07:42:39 +0545

    added a missing file

---
+++ char_q3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cctype>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    int v = 0, c = 0;
+    for (char ch : s) {
+        ch = tolower(ch);
+        if (isalpha(ch)) {
+            if (ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+                v++;
+            else
+                c++;
+        }
+    }
+
+    cout << "Vowels: " << v << endl;
+    cout << "Consonants: " << c << endl;
+
+    return 0;
+}
